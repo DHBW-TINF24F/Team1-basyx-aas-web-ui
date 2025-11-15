93ae2fe1be7d9960a6141f7a6aecb871f8177912
ChrisTopher <inf24268@lehre.dhbw-stuttgart.de>
2025-11-15 12:23:42 +0100

    Detail view from Priyanshu

--- aas-web-ui/src/pages/modules/CDViewer.vue
+++ aas-web-ui/src/pages/modules/CDViewer.vue
@@ -1,6 +1,7 @@
 <template>
     <v-container fluid class="pa-0">
         <!-- Eure komponenten hier einfügen z.B Komponente = MyComponent.vue, dann <MyComponent></MyComponent> hier rein packen-->
+        <ConceptDescriptionDetailView></ConceptDescriptionDetailView>
     </v-container>
 </template>
 
